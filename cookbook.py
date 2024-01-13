ed255638d3420c124c5d77eb2df4486c17575bd2
Igor Kobelev <kia1809@yandex.ru>
2024-01-13 21:06:08 +0300

    finished task 2

--- cookbook.py
+++ cookbook.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
 
-def get_ingredients(file, num_ingredients):
+def get_ingredient(file, num_ingredients):
     all_ingredients = []
     keys = ['ingredient_name', 'quantity', 'measure']
     for _ in range(num_ingredients):
@@ -18,15 +18,28 @@ def create_cook_book(f_name):
             recipe_name = line.strip()
             if recipe_name:
                 num_ingredients = int(file_input.readline())
-                ingredients = get_ingredients(file_input, num_ingredients)
+                ingredients = get_ingredient(file_input, num_ingredients)
                 book[recipe_name] = ingredients
     return book
 
 
 def get_shop_list_by_dishes(dishes, person_count):
     list_by_dishes = {}
+    for dish in dishes:
+        for ingredient in cook_book.get(dish):
+            product = list_by_dishes.setdefault(
+                ingredient['ingredient_name'],
+                {})
+            if not product:
+                product['measure'] = ingredient['measure']
+                product['quantity'] = ingredient['quantity'] * person_count
+            else:
+                product['quantity'] += ingredient['quantity'] * person_count
+    return list_by_dishes
 
 
 file_name = 'sample_recipes.txt'
 cook_book = create_cook_book(file_name)
 pprint(cook_book)
+shop_list = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+pprint(shop_list)
